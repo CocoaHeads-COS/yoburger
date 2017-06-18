f8272ad843b55cd942f20b18627865295a9d8ffa
shanecowherd <shane.cowherd@me.com>
2017-06-17 21:34:12 -0600

    Updated message api, now shows YoBuger username.

--- server/Sources/YoBurgerMain/YoBurgerBot.swift
+++ server/Sources/YoBurgerMain/YoBurgerBot.swift
@@ -29,13 +29,15 @@ class YoBurgerBot {
       }
       
       if messageSenderID != yoburgerID {
-        
-        if message.uppercased().contains("BURGERHISTORY") && referrers.count > 0 {
+        if message.uppercased().contains("DEBUG") {
+          self?.sendMessage(channel: channel, message: "Debug :)")
+          
+        } else if message.uppercased().contains("BURGERHISTORY") && referrers.count > 0 {
           for referrer in referrers {
             let referrerName = self?.allUsersInChannel[referrer] ?? "Unknown"
             let referrerCount = self?.users[referrer]?.history.count ?? 0
             let messageToSend = "@\(referrerName) has \(referrerCount) :hamburger:s"
-            self?.bot.webAPI?.sendMessage(channel: channel, text: messageToSend, success: nil, failure: nil)
+            self?.sendMessage(channel: channel, message: messageToSend)
           }
           
         } else {
@@ -63,7 +65,7 @@ class YoBurgerBot {
               if refburgercount > 1 {
                 ifplural = "s"
               }
-              self?.bot.webAPI?.sendMessage(channel: channel, text: "Woot! @\(username) has given @\(refusername) a :hamburger:.\n@\(refusername) now has \(refburgercount) :hamburger:\(ifplural)", success: nil, failure: nil)
+              self?.sendMessage(channel: channel, message: "@\(username) has given @\(refusername) a :hamburger:.\n@\(refusername) now has [ \(refburgercount) ] :hamburger:\(ifplural)")
             }
           }
           //Debug Log
@@ -71,6 +73,10 @@ class YoBurgerBot {
         }
       }
     }
+  }
+  
+  func sendMessage(channel:String, message:String) {
+    self.bot.webAPI?.sendMessage(channel: channel, text: message, username: "YoBurger", asUser: true, parse: nil, linkNames: true, attachments: nil, unfurlLinks: nil, unfurlMedia: nil, iconURL: nil, iconEmoji: nil, success: nil, failure: nil)
   }
   
   func saveAllUserNamesInChannel(event:Event, client:Client?) -> Bool {
